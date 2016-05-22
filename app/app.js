6db196a32b6c60e11b38419aeebb087b9b7cffff
beiatisi <mail@beiatisi.com>
2016-05-22 22:31:55 +0800

    add news route

--- app/app.js
+++ app/app.js
@@ -3,6 +3,10 @@ app.get('/',function(req, res) {
    res.end("Home");
 });
 
+app.get('/news',function(req, res) {
+   res.end("news");
+});
+
 app.get('/test',function(req, res) {
    res.end("Hello world.");
 });
